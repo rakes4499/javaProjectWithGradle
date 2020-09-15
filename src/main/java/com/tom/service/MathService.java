e7a5b412dd63148fbf8c897c962b771867d072ef
Rakesh Kumar <30525754+rakes4499@users.noreply.github.com>
2020-09-16 00:26:55 +0530

    Add files via upload

---
+++ src/main/java/com/tom/service/MathService.java
@@ -0,0 +1,15 @@
+package com.tom.service;
+
+public class MathService {
+    public int multiply(int firstNumber, int secondNumber) {
+
+        return firstNumber * secondNumber;
+    }
+
+    public int subtract(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+    public static void main(String [] args) {
+        System.out.println("this is fu******* working");
+    }
+}
